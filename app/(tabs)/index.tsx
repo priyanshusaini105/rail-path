6e07078ff445dc68840d3f0067a861dbbdfa8d48
krish <sharmageeta847@gmail.com>
2024-09-01 17:05:24 +0530

    auto location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,50 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, Platform, View , Alert, PermissionsAndroid} from 'react-native';
 import Mapbox, { type SymbolLayerStyle } from '@rnmapbox/maps';
+import Geolocation from '@react-native-community/geolocation';
+import * as Location from 'expo-location';
 
 Mapbox.setAccessToken("sk.eyJ1IjoicHJpeWFuc2h1c2FpbmkiLCJhIjoiY20wamE0MDIwMHFqajJsc2xvOGVvNzRwZSJ9.PcYp0iqTBPHPSXuJ9EWLZQ");
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState<number[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert("Permission to access location was denied");
+        return;
+      }
+
+      let userLocation = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = userLocation.coords;
+      setLocation([longitude, latitude]);
+    })();
+  }, []);
   return (
     <View style={styles.page}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      {location ? (
+        <Mapbox.MapView style={styles.map}>
+          <Mapbox.Camera
+            zoomLevel={14}
+            centerCoordinate={location}
+          />
+          <Mapbox.PointAnnotation
+            id="userLocation"
+            coordinate={location}
+          >
+            <View>
+              <View />
+            </View>
+          </Mapbox.PointAnnotation>
+        </Mapbox.MapView>
+      ) : (
         <Mapbox.MapView style={styles.map} />
-      </View>
+      )}
     </View>
+  </View>
   );
 }
 
@@ -20,8 +55,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   container: {
-    height: 300,
-    width: 300,
+    height: 800,
+    width: 400,
   },
   map: {
     flex: 1
