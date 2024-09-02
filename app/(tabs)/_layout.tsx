a44bb04b6f16f01b7677403b86ad792cbd7dde8a
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-09-02 17:23:36 +0530

    chore: Update Map component to use rnmapbox package

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="Map"
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
